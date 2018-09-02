f91d8d11550fff97cf52a612d36e1c12d651b1a3
Diana Dias <dianadiasds@gmail.com>
2018-09-02 00:03:36 -0300

    second commit

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -3,9 +3,8 @@ import Main from '../templates/Main'
 
 export default props =>
 
-    <Main icon="home" title="Início" subtitle="Segundo Projeto do capítulo de React.">
-        <div className='display-4'>Bem Vindo!</div>
+    <Main icon="home" title="Início" subtitle="Página Inicial">
+        <div className='display-4'>Crud React</div>
         <hr />
-        <p className="mb-0">Sistema para exemplificar a construção de um cadastro desenvolvido em React!</p>
+        <p className="mb-0">Crud usando react </p>
     </Main>
-
